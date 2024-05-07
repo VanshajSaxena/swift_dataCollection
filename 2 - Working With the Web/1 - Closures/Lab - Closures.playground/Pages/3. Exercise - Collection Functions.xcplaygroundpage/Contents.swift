f11014ca39bc3c91860916ae1c038470a45fee25
Vanshaj Saxena <vs110405@outlook.com>
2024-05-07 12:24:27 +0530

    Closures Completed

--- 2 - Working With the Web/1 - Closures/Lab - Closures.playground/Pages/3. Exercise - Collection Functions.xcplaygroundpage/Contents.swift
+++ 2 - Working With the Web/1 - Closures/Lab - Closures.playground/Pages/3. Exercise - Collection Functions.xcplaygroundpage/Contents.swift
@@ -4,16 +4,16 @@
  Using the code below, use the `map` function to create an array of `Int` values, whose values are equal to the original integer value, plus 1. Use `$0` as you iterate through the values of the array. Print the resulting collection.
  */
 let testScores = [65, 80, 88, 90, 47]
-
-
+var newScores = testScores.map {$0 + 1}
+print(newScores)
 //:  Using the code below, use the `filter` function to create a new array of `String` values. The new array should only include Strings longer than four characters. Use `$0` as you iterate through the values of the array.  Print the resulting collection.
 let schoolSubjects = ["Math", "Computer Science", "Gym", "English", "Biology"]
-
-
+var someSubjects = schoolSubjects.filter {$0.count < 4}
+print(someSubjects)
 //:  Using the code below, use the `reduce` function to subtract all of the values within the array from the starting value 100. Print the resulting value.
 let damageTaken = [25, 10, 15, 30, 20]
-
-
+var overallDamage = 100 - damageTaken.reduce(0) {$0+$1}
+print(overallDamage)
 /*:
  _Copyright © 2023 Apple Inc._
 
